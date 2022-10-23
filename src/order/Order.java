74b92c0b00c27dac5fd3ce5ca3ef2207ec3070a0
Oleksandr <oleksandrpopovych89@gmail.com>
2022-10-23 22:59:29 +0300

    Initial commit

---
+++ src/order/Order.java
@@ -0,0 +1,24 @@
+package order;
+
+public class Order {
+    private long id;
+
+    public Order(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                '}';
+    }
+}
---
+++ src/order/OrderService.java
@@ -0,0 +1,52 @@
+package order;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class OrderService {
+    private volatile List<Order> orders;
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public List<Order> findAll() {
+        try {
+            lock.readLock().lock();
+            return orders;
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    public List<Order> findLast100() {
+
+        try {
+            lock.readLock().lock();
+            return orders.stream()
+                    .sorted(Comparator.comparingLong(Order::getId))
+                    .limit(100)
+                    .toList();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    public void add(Order order) {
+        try {
+            lock.readLock().lock();
+            orders.add(order);
+        } finally {
+            lock.readLock().unlock();
+        }
+
+    }
+
+    public void remove(Order order) {
+        try {
+            lock.readLock().lock();
+            orders.remove(order);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+}
